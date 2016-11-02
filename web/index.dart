61dad1af50273aab29b25e351c927a95b7cec346
Jacob MacDonald <jakemac53@gmail.com>
2016-11-02 12:53:04 -0700

    create an instance and check type

--- web/index.dart
+++ web/index.dart
@@ -2,6 +2,9 @@ import 'dart:html';
 
 main() async {
   document.registerElement('my-element', MyElement, extendsTag: 'template');
+  var e = new Element.tag('template', 'my-element');
+  document.body.append(e);
+  print(e is TemplateElement);
 }
 
 class MyElement extends TemplateElement {
